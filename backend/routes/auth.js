a026fbdefcc72c170142d24ac576606fe80450c7
poudyalamit <poudyalamit20@gmail.com>
2023-04-18 22:08:41 +0545

    added the create user endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,26 +3,38 @@ const User = require('../models/User');
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
 
-//Create a User using: POST '/api/auth/'. Doesnot require Auth
-router.post('/', [
+//Create a User using: POST '/api/auth/createuser'.No Login required
+router.post('/createuser', [
     body('firstname', "Enter a valid first name").isLength({ min: 3 }),
     body('lastname', "Enter a valid last name").isLength({ min: 3 }),
     body('email', "Enter a valid E-mail").isEmail(),
     body('password', "Password must be at least 5 characters").isLength({ min: 5 }),
-], (req, res) => {
+], async (req, res) => {
+    //if there are errors return bad request and errors
     let success = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ success, errors: errors.array() });
     }
-    User.create({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        password: req.body.password
-    }).then(user=>res.json(user))
-    .catch(err=>{console.log(err)
-    res.json({error: "Please enter a unique value for email",message: err.message})})
+    //Check whether the user with this email exists already
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry the user with this email already exists" })
+        }
+        //this will wait and create a new user
+        user = await User.create({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
+
 })
 
 
