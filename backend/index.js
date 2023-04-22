2bf1a91aa51c89c38d91ad7b5dd0671ffbba8cd5
nepalankit <nepalankit16@gmail.com>
2023-04-22 15:47:33 +0545

    added products

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
-const connectToMongo=require('./db');
+const connectToMongo = require("./db");
 connectToMongo();
-const express = require('express')
-const app = express()
-const port = 5000
-app.use(express.json())
-//Available Routes 
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/roomdesc',require('./routes/roomdesc'))
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const product = require("./routes/productRoute");
+const express = require("express");
+const app = express();
+const port = 5000;
+app.use(express.json());
+//Available Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/roomdesc", require("./routes/roomdesc"));
+// app.use("/api/v1", product);
+app.use("/api/products", require("./routes/products"));
 
 app.listen(port, () => {
-  console.log(`House-Rent backend running at http://localhost:${port}`)
-})
+  console.log(`House-Rent backend running at http://localhost:${port}`);
+});
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const router = express.Router();
 const {
   getAllProducts,
   createProduct,
@@ -6,9 +7,6 @@ const {
   deleteProduct,
   getProductDetails,
 } = require("../controllers/productControllers");
-const router = express.Router();
-router.route("/products").get(getAllProducts);
-router.route("/product/new").post(createProduct);
 
 router
   .route("/product/:id")
